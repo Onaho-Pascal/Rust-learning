b55314803272a64b5b9c2657fab6bc41eba4b250
Pascal <Pascalonaho@gmail.com>
2025-08-15 10:43:45 +0100

    updated

--- my_project/src/main.rs
+++ my_project/src/main.rs
@@ -1,3 +1,11 @@
+const PRICE: u32 = 50;
 fn main() {
-    println!("Hello, world!");
+    let x = "denim";
+    let mut y = "jacket";
+    println!("The cost of the {x} {y} is {PRICE}!");
+    y = "gucci";
+    let z = PRICE + 100;
+    println!("While the cost of the {y} is {z}");
+//    let y: u32 = 64;
+//     println!("{y}");
 }
