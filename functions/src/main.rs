acbeb954ba966b4dd4e1b8db38d38ff1b43f1a7c
Pascal <Pascalonaho@gmail.com>
2025-08-15 16:52:33 +0100

    updated

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,14 @@
+const KNOCK_OUT: u32 = 200;
+
 fn main() {
-    println!("Hello, world!");
-    purchase();
+
+    let knock_out: u32 = 200;
+    println!("The maximum damage achievable is {knock_out}");
+    // knock_out = 300;
+    // println!("The maximum damage achievable is {knock_out}");
+    println!("The maximum damage achievable is {KNOCK_OUT}");
+    println!("At {KNOCK_OUT} damage, Player gets knocked out!");
 }
 
-fn purchase() {
-    println!("I love Bioinformatics");
-}
+
+
