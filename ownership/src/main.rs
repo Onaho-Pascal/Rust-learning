6e78927cce3952066cb285c2f6938fbe4ce3a999
Pascal <Pascalonaho@gmail.com>
2025-08-23 23:41:13 +0100

    updated

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,19 +1,19 @@
 // Moving ownership
 fn main() {
-//     let s1 = String::from("hello");
-//     let s2 = s1; // ownership moves
+    let s1 = String::from("hello");
+    let s2 = s1; // ownership moves
 
-//     // println!("{}", s1); ❌ invalid, s1 no longer owns the string
-//     println!("{}", s2);
+    // println!("{}", s1); ❌ invalid, s1 no longer owns the string
+    println!("{}", s2);
 
-// // Borrowing Ownership
+// Borrowing Ownership
 
-//     fn length(s: &String) -> usize {
-//         s.len()
-//     }
-//     let s1 = String::from("hello");
-//     let len = length(&s1); // borrow, don’t take ownership
-//     println!("'{}' is {} chars long", s1, len);
+    fn length(s: &String) -> usize {
+        s.len()
+    }
+    let s1 = String::from("hello");
+    let len = length(&s1); // borrow, don’t take ownership
+    println!("'{}' is {} chars long", s1, len);
 
     let book = String::from("The Rust Programming Language");
 
