0551d8a50d735f0aeef5ec2c94710b0ee0b3f7b8
Pascal <Pascalonaho@gmail.com>
2025-08-30 18:20:20 +0100

    updated

---
+++ test_/target/debug/deps/test_.d
@@ -0,0 +1,5 @@
+C:\Users\user\Desktop\Rust files\Rust-learning\test_\target\debug\deps\test_.d: src\main.rs
+
+C:\Users\user\Desktop\Rust files\Rust-learning\test_\target\debug\deps\test_.exe: src\main.rs
+
+src\main.rs:
