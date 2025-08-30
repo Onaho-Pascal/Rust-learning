e3438483efad0d0b6de7f73457821c9a103039c2
Pascal <Pascalonaho@gmail.com>
2025-08-30 19:31:35 +0100

    updated

---
+++ traits_/target/debug/deps/traits_.d
@@ -0,0 +1,5 @@
+C:\Users\user\Desktop\Rust files\Rust-learning\traits_\target\debug\deps\traits_.d: src\main.rs
+
+C:\Users\user\Desktop\Rust files\Rust-learning\traits_\target\debug\deps\traits_.exe: src\main.rs
+
+src\main.rs:
---
+++ traits_/target/debug/traits_.d
@@ -0,0 +1,1 @@
+C:\Users\user\Desktop\Rust\ files\Rust-learning\traits_\target\debug\traits_.exe: C:\Users\user\Desktop\Rust\ files\Rust-learning\traits_\src\main.rs
