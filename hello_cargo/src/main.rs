9dd95f66dd2e9b57a321a071c7dd954b8476e510
unknown <Pascalonaho@gmail.com>
2026-03-08 16:53:35 +0100

    updated

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,12 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world! I am here to start coding in rust");
+    let stdout =stdout();
+    let message = String::from("Hello Fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
+
 }
